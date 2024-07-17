2acfa7b80a142990b0e20b939dec2d4892e45999
vadim <ivadimn@mail.ru>
2024-07-17 17:28:19 +0300

    update shm

--- shm/prep.c
+++ shm/prep.c
@@ -4,8 +4,10 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <errno.h>
+#include <unistd.h>
 
 #define MEM_SIZE 1000
+#define SHM_MODE 0600
 
 int main(int argc, char** argv)
 {
@@ -15,27 +17,44 @@ int main(int argc, char** argv)
     int *sh2 = NULL;
 
     source1 = ftok(argv[0], 99);
-    source1 = ftok(argv[0], 199);
+    source2 = ftok(argv[0], 199);
+    printf("key 1 = %d, key 2 = %d\n", source1, source2);
 
-    shm_id1 = shmget(source1, MEM_SIZE, IPC_CREAT);
+    shm_id1 = shmget(source1, MEM_SIZE, SHM_MODE | IPC_CREAT) ;
     if (shm_id1 == -1)
     {
-        printf("Error with creating  shared memory area\n", strerror(errno));
+        printf("Error with creating shared memory area 1 %d: %s\n", source1, strerror(errno));
         exit(EXIT_FAILURE);
     }
-    shm_id2 = shmget(source2, MEM_SIZE, IPC_CREAT);
+    printf("id1 = %d\n", shm_id1);
+    shm_id2 = shmget(source2, MEM_SIZE, SHM_MODE | IPC_CREAT);
     if (shm_id2 == -1)
     {
-        printf("Error with creating  shared memory area\n", strerror(errno));
+        printf("Error with creating shared memory area 2 %d: %s\n", source2, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    printf("id2 = %d\n", shm_id2);
+
+    sh1 = (int*) shmat(shm_id1, sh1, 0);
+    if (sh1 == (void*)-1)
+    {
+        printf("Error with addressing shared memory area: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    sh2 = (int*) shmat(shm_id2, sh2, 0);
+    if (sh2 == (void*) -1)
+    {
+        printf("Error with addressing shared memory area: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    sh1 = (int*) shmat(shm_id1, NULL, SHM_RND)
-    
-
-
-
-
+    printf("address1 = %p, address2 = %p\n", (void*) sh1, (void*) sh2);
+    for (size_t i = 0; i < 10; i++)
+    {
+        sh1[i] = i * 2;
+        sh2[i] = i * 2 + 10;
+    }
+ 
     while (1)
     {
         usleep(500000);
--- shm/solution.c
+++ shm/solution.c
@@ -3,24 +3,50 @@
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <errno.h>
 
-int main(int argc, char** argv)
+#define MEM_SIZE 1000
+#define SHM_MODE 0600
+#define SHM_MODE_READ 0400
+
+int* get_shmem(key_t key, size_t size, int flag) 
 {
-    key_t source1, source2, dest;
-    
-    if (argc != 3)
+    int *buf = NULL;
+    int id;
+    id = shmget(key, size, flag);
+    if (id == -1)
     {
-        printf("Usage %s key_memory1 key_memory2\n", argv[0]);
+        printf("Error creating shared memory with key %x: %s\n", key, strerror(errno));
         exit(EXIT_FAILURE);
     }
-
-    source1 = atol(argv[1]);
-    source1 = atol(argv[2]);
-    
-    while (1)
+    buf = (int*) shmat(id, buf, 0);
+    if (buf == (void*)-1)
     {
-        usleep(500000);
+        printf("Error with addressing shared memory with key %x: %s\n", key, strerror(errno));
+        exit(EXIT_FAILURE);
     }
+    return buf;
+}
+
+
+int main(/*int argc, char** argv*/)
+{
+    key_t source1, source2, dest_key;
+    int *sh1 = NULL;
+    int *sh2 = NULL;
+    int *dest = NULL; 
+
+    source1 = 0x630202cf; //atol(argv[1]);
+    source2 =  0xc70202cf; //atol(argv[2]);
+
+    sh1 = get_shmem(source1, 0, SHM_MODE_READ);
+    sh2 = get_shmem(source2, 0, SHM_MODE_READ);
+
+    for (size_t i = 0; i < 100; i++)
+    {
+       printf("sh1: %d, sh2: %d\n", sh1[i], sh2[i]);
+    }
+     
 
     return 0;
 }
